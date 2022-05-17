d9c397d2ce7ce13d02a43516793ad14480011816
Torma Kristóf <tormakristof@tormakristof.eu>
2022-05-18 01:36:01 +0200

    return an integer

--- hanyadikhetvan/__init__.py
+++ hanyadikhetvan/__init__.py
@@ -1,7 +1,10 @@
+from math import floor
+from datetime import date
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    startdate = date(2022, 2, 6)
     return func.HttpResponse(
-        "test",
+        floor((date.today() - startdate).days / 7),
         status_code=200)
