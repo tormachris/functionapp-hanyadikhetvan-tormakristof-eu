60aae481bc9a127aec51c3bea2e8aedfc0085f9c
Torma Kristóf <tormakristof@tormakristof.eu>
2022-05-18 01:32:25 +0200

    return a string

--- hanyadikhetvan/__init__.py
+++ hanyadikhetvan/__init__.py
@@ -1,9 +1,7 @@
-from datetime import date
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    startdate = date(2022, 2, 6)
     return func.HttpResponse(
-        (date.today() - startdate).days / 7,
+        "test",
         status_code=200)
