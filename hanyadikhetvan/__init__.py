0004ecf6967d4937c2584cdd7818945f65958c4d
Torma Kristóf <tormakristof@tormakristof.eu>
2022-05-18 01:37:50 +0200

    convert integer to string

--- hanyadikhetvan/__init__.py
+++ hanyadikhetvan/__init__.py
@@ -5,6 +5,7 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     startdate = date(2022, 2, 6)
+    currentweek = floor((date.today() - startdate).days / 7)
     return func.HttpResponse(
-        floor((date.today() - startdate).days / 7),
+        str(currentweek),
         status_code=200)
